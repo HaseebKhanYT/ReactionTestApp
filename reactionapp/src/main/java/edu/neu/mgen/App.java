4b887d0ffe27eec7bae7eada753aa0d229de4583
HaseebKhanYT <63363319+HaseebKhanYT@users.noreply.github.com>
2023-12-12 00:09:04 +0530

    Add files via upload

---
+++ reactionapp/src/main/java/edu/neu/mgen/App.java
@@ -0,0 +1,17 @@
+package edu.neu.mgen;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
